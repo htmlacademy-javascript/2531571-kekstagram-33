d815dadc6402b71eb1980f933c413c5cc3a8608b
Миронов Александр <mironovav1993@gmail.com>
2024-10-21 21:04:20 +0400

    функция длины и палиндрома

---
+++ js/function.js
@@ -0,0 +1,34 @@
+/*
+// Строка короче 20 символов
+имяФункции('проверяемая строка', 20); // true
+// Длина строки ровно 18 символов
+имяФункции('проверяемая строка', 18); // true
+// Строка длиннее 10 символов
+имяФункции('проверяемая строка', 10); // false
+*/
+
+const checkLength = (text,number) => {
+  if(length(text) <= number) {
+    return true;
+  }else {
+    return false;
+  }
+};
+
+checkLength('проверяемая строка',20);
+
+
+const palindrom = (string) => {
+  const charset = string.toUpperCase().replaceAll(' ',''); //тут мы приводим к стандарту вводимую строку
+  let reverseCharset; //это переменная для хранения символов, считываемых ниже.тут вопрос про инициализацию переменной
+  for (let i = charset.at(-1);i > 0;i--) {
+    reverseCharset += i;
+    if (charset === reverseCharset) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+palindrom('топот');
